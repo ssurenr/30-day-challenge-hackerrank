1dcac8cb7ba1de73ec0e95aa7ffd38dc2a971919
Suren <surendhar@ymail.com>
2017-11-21 00:02:30 +0530

    Add Challenge 2

---
+++ Challenge2.py
@@ -0,0 +1,23 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+
+# Declare second integer, double, and String variables.
+ui = 0
+ud = 0.0
+us = ''
+
+# Read and save an integer, double, and String to your variables.
+ui = int(input())
+ud = float(input())
+us = input()
+
+# Print the sum of both integer variables on a new line.
+print(i + ui)
+# Print the sum of the double variables on a new line.
+print(round(d + ud, 1))
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + us)
+
+
